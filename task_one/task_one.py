5fd0355c83d9cd04007ad59025871b8f402237da
Serhii Stefanovskyi <s.stefanovskyi@gms-worldwide.com>
2021-08-17 15:29:06 +0300

    Fix task 1

--- task_one/task_one.py
+++ task_one/task_one.py
@@ -1,12 +1,5 @@
-my_list = [8, 2, 3, 1, 5, 6, 2]
-lesser = 3
-my_list.sort()
-
-
-def slice_less():
+def slice_less(my_list, lesser):
+    my_list.sort(reverse=True)
     for i in range(len(my_list)):
         if my_list[i] > lesser:
             print(my_list[i])
-
-
-slice_less()
